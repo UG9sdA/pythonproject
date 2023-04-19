68d8c90ab2b5261f91e010e598836fffb2d3ad4d
UG9sdA <tnmnhanh@gmail.com>
2023-04-19 00:31:47 -0400

    Final changes

--- Courses.py
+++ Courses.py
@@ -1,34 +1,40 @@
 class Course:
     def __init__(self, course_name):
         self.__course_students = {}
-	self.__course_name = course_name
+        self.__course_name = course_name
+
+    def infos(self):
+        return str(self.__course_students).replace('\'','"').replace(' ','') + ' ' + self.__course_name
+
+    def set_students(self,diction):
+        self.__course_students = diction
+
     def get_course_name(self):
         return self.__course_name
-    def get_course(self):
-        return f'Name: {self.__course_info[0]}'
 
-    def set_attendance(self,int: lecture, student_id, present):   #Attendance of student in each course
-        self.__course_students[student_id][lecture-1] = present
+    def set_mark(self,student_id,att,mid,fin):
+        self.__course_students[student_id] = [att,mid,fin]
+
     def get_attendance(self, student_id):
-        return sum(self.__course_students[student_id][0:10])
+        return self.__course_students[student_id][0]
 
-    def set_midTerm(self, student_id, mark):  #Midterm of student in each course
-        self.__course_students[student_id][10] = mark
     def get_midTerm(self, student_id):
-        return self.__course_students[student_id][10]
+        return self.__course_students[student_id][1]
 
-    def set_final(self, student_final, id):  #Midterm of student in each course
-        self.__course_info[1][student_id][11] = mark
     def get_final(self, student_id):
-        return self.__course_students[student_id][11]
+        return self.__course_students[student_id][2]
 
-    def add_student(self,student_id):
-	if student_id in self.__course_students:
-	    overwrite = input("Student ID already exist in this course, do you want to overwrite?")
-	    if overwrite != 1:
-		return 0
-	self.__course_students[student_id] = [0]*12
-    def show_student():
-	for student in self.__course_students:
-	    print(f"Student ID: {student} - Attendance: {get_attendance(student)} - Mid: {get_midtearm(student)} Final: {get_final(student)} ")
+    def add_student(self, student_id):
+        self.__course_students[student_id] = [0] * 3
 
+    def show_student_info(self):
+        return self.__course_students
+
+    def show_total_student(self):
+        return len(self.__course_students)
+
+    def remove_student(self,student_id):
+        self.__course_students.pop(student_id)
+
+    def get_total(self,student_id):
+        return round(int(self.get_attendance(student_id))*0.1+int(self.get_midTerm(student_id))*0.4+int(self.get_final(student_id))*0.5,2)
